4910d918013f05e54fb7d97a510841e6fbe229c5
Adriano Leal <adbrum@outlook.com>
2023-07-06 22:00:03 +0100

    Created component Drinks

---
+++ components/Drinks/Drinks.jsx
@@ -0,0 +1,172 @@
+import React from "react";
+
+const Drinks = () => {
+  return (
+    <>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Bebidas</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Sangria 1L</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>8€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Sangria 1/2 Litro</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>4,50€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Água 1,5 Litro</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1,50€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Água 0,5 Litro</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Águas com gás (Pedras, Castelo, Tónica)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1,20€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Pepsi 1.25l</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>3€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Pepsi / Seven-up / Sumol</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1,50€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Ice-Tea 0,33l</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1,50€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Cerveja 0,33l</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1,30€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Cerveja mini 0,20l</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Cerveja sem álcool</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1,50€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Somerby</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>2,20€</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Cafetaria</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Café / descafeinado / garoto / carioca de limão</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>0,70€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Chá / meia de leite / abatanado</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>0,90€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Carioca de café</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>0,65€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Galão / Capuccino</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1,20€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Café duplo</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1,40€</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Drinks;
