9cfb1988d4395af019e7fb6abe0e6f23eb001c82
Adriano Leal <adbrum@outlook.com>
2023-07-06 21:45:55 +0100

    Created component Grilled

---
+++ components/Grilled/Grilled.jsx
@@ -0,0 +1,263 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Grilled = () => {
+  return (
+    <>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Porco Preto</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/secretos_porco.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Secreto de porco preto</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1/2 dose 8,50€ / 1 dose 10€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/espetadas_secretos.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Espetada de Secreto de porco preto</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>13€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/lagartos_porco_preto.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Lagartos de porco preto</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>9,50€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/lombinhos_porco_preto.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Lombinho de porco preto</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>10€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Costeletas do cachaço de porco preto</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>10€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Selecção de porco preto na grelha (2 pax)</h3>
+              <p>secreto / lombinho / costeletas</p>
+              <h5>19€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Selecção de porco preto na grelha (3 pax)</h3>
+              <p>secreto / lombinho / costeletas</p>
+              <h5>28€</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Porco</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Lombo de porco</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1/2 dose 7€ / 1 dose 8,50€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Febras de Porco</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>1/2 dose 6€ / 1 dose 7,50€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Salsicha Fresca</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>9,50€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Bitoque de porco c/ batata e ovo</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>7€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/mista_porco.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Mista de Porco na grelha</h3>
+              <p>febra / costeleta / salsicha</p>
+              <h5>9€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Mista de Porco na grelha (2 pax)</h3>
+              <p>febra / costeleta / salsicha</p>
+              <h5>17€</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Borrego</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/costeletas_borrego.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Costeletas de Borrego</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>11€</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Lombo de novilho</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Posta de novilho à 25 de Abril</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>19€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/posta_novilho.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Posta de novilho</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>18€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Espetada de novilho</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>18€</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/bife_novilho.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Bife de novilho</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>17€</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Aves</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img
+              src="/image/grelhados/febras_peru.jpg"
+              className="mr-3"
+              alt="..."
+            />
+            <div className="media-body align-self-center">
+              <h3>Febras de Peru</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>8€</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Espetadas de Peru</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>12€</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Grilled;
