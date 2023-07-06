1ca2a6d548174e953ee0ba55d18bd66171c2270b
Adriano Leal <adbrum@outlook.com>
2023-07-06 22:10:06 +0100

    Created component WhiteWine

---
+++ components/WhiteWine/WhiteWine.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+
+const WhiteWine = () => {
+  return (
+    <>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Vinhos Brancos / Alentejo</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Vinho Branco da Casa (vinho de mesa Vila de Frades)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>2€ (1/4l) / 3€ (1/2l) / 5€ (1l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Monte da Peceguina</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>16€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Flor do Guadiana</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>16€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Nã te rales</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>12€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Sobro</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>10€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Black (Mingorra)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>29€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Monte Velho</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>9€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Mingorra</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>12€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Vidigueira Antão Vaz (inspiração ato IV )</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>5€ (0,38l) / 9€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Terras d’uva</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>6€ (0,38l) / 10€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Paço do conde</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>10€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WhiteWine;
