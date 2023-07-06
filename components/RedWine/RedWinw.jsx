31bd65465dcef04f385334cea669a55eac08becf
Adriano Leal <adbrum@outlook.com>
2023-07-06 22:05:55 +0100

    Created component RedWine

---
+++ components/RedWine/RedWinw.jsx
@@ -0,0 +1,252 @@
+import React from "react";
+
+const RedWine = () => {
+  return (
+    <>
+      <div className="col-lg-5">
+        <div className="section_tittle">
+          <h2>Vinhos Tintos / Alentejo</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Vinho Tinto da Casa (vinho de mesa Vila de Frades)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>2€ (1/4l) / 3€ (1/2l) / 5€ (1l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Monte da Peceguina</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>9€ (0,38l) / 16€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Herdade dos Grous</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>16€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Cantando (herd. da Figueirinha)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>16€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Nã te rales (herd. da Figueirinha)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>7,50€ (0,38l) / 12€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Black (Mingorra)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>29€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Mingorra</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>12€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Terras D’Uva</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>5€ (0,38l) / 9€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Monte Velho</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>5€ (0,38l) / 9€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Chaminé</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>6€ (0,38l) / 10€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Marquês de Borba</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>7€ (0,38l) / 11€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Convento da Tomina</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>15€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Sobro</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>10€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Paço do Conde</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>10€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Herd. Grande Gerações</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>22€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Herd. Grande</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>12€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Vidigueira Grande escolha (A Decisão ato V)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>14€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Vidigueira (Saudade ato III )</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>9€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Trinca Bolotas</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>11€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Monte das Servas</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>12€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-6">
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Caiado (adega Mayor)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>9€ (0,75l)</h5>
+            </div>
+          </div>
+          <div className="single_food_item media">
+            <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+            <div className="media-body align-self-center">
+              <h3>Vinha das lebres (Paulo Laureano)</h3>
+              {/* <p>They're wherein heaven seed hath nothing</p> */}
+              <h5>9€ (0,75l)</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-5">
+          <div className="section_tittle">
+            <h2>Reservas</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6 col-lg-6">
+            <div className="single_food_item media">
+              <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+              <div className="media-body align-self-center">
+                <h3>Conde D’ervideira reserva</h3>
+                {/* <p>They're wherein heaven seed hath nothing</p> */}
+                <h5>22€ (0,75l)</h5>
+              </div>
+            </div>
+            <div className="single_food_item media">
+              <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+              <div className="media-body align-self-center">
+                <h3>Fonte Mouro reserva (herd. da Figueirinha)</h3>
+                {/* <p>They're wherein heaven seed hath nothing</p> */}
+                <h5>18€ (0,75l)</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-6">
+            <div className="single_food_item media">
+              <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+              <div className="media-body align-self-center">
+                <h3>Herdade do Sobroso reserva</h3>
+                {/* <p>They're wherein heaven seed hath nothing</p> */}
+                <h5>16€ (0,75l)</h5>
+              </div>
+            </div>
+            <div className="single_food_item media">
+              <img src="/image/food_menu/logo.png" className="mr-3" alt="..." />
+              <div className="media-body align-self-center">
+                <h3>Adega Mayor reserva</h3>
+                {/* <p>They're wherein heaven seed hath nothing</p> */}
+                <h5>16€ (0,75l)</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RedWine;
